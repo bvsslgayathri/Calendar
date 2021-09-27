99fd3971682b5c28a923f4406c21f91c3537a5d8
samba <74446747+samba-chinta@users.noreply.github.com>
2021-09-27 10:29:55 +0530

    Data Added

--- script.js
+++ script.js
@@ -3,6 +3,87 @@ let modalContainer = document.getElementById('myModal');
 let topPos = document.getElementById("16:00").offsetTop + 30;
 let isEmpty = 1;
 
+const day1 = [
+  {
+    name: "EPICS Day - 1",
+    startTime: "17:30",
+    endTime: "19:00",
+    link: "http://ieeesbgvpce.org/",
+    speaker: "Mr.abc",
+  }
+]
+
+const day2 = [
+  {
+    name: "EPICS Day-2",
+    startTime: "17:30",
+    endTime: "19:00",
+    link: "http://ieeesbgvpce.org/",
+    speaker: "Mr.abc",
+  }
+]
+
+const day3 = [
+  {
+    name: "Explore your passion workshop",
+    startTime: "16:30",
+    endTime: "17:30",
+    link: "http://ieeesbgvpce.org/",
+    speaker: "Mr.abc",
+  },
+  {
+    name: "Web Dev Workshop",
+    startTime: "18:00",
+    endTime: "19:30",
+    link: "http://ieeesbgvpce.org/",
+    speaker: "Mr.abc",
+  }
+]
+
+const day4 = [
+  {
+    name: "Simulation Tool",
+    startTime: "16:30",
+    endTime: "17:30",
+    link: "http://ieeesbgvpce.org/",
+    speaker: "Mr.abc",
+  },
+  {
+    name: "Web Development",
+    startTime: "18:00",
+    endTime: "19:30",
+    link: "http://ieeesbgvpce.org/",
+    speaker: "Mr.abc",
+  }
+]
+
+const day5 = [
+  {
+    name: "Explore your passion workshop",
+    startTime: "16:30",
+    endTime: "17:30",
+    link: "http://ieeesbgvpce.org/",
+    speaker: "Mr.abc",
+  },
+  {
+    name: "Web Development",
+    startTime: "18:00",
+    endTime: "19:30",
+    link: "http://ieeesbgvpce.org/",
+    speaker: "Mr.abc",
+  }
+]
+
+const day6 = [
+  {
+    name: "Coding Competition",
+    startTime: "16:30",
+    endTime: "19:30",
+    link: "http://ieeesbgvpce.org/",
+    speaker: "Mr.abc",
+  }
+]
+
 const day7 = [
   {
     name: "Linkedin or Resume",
@@ -20,25 +101,31 @@ const day7 = [
   }
 ]
 
-const day4 = [
+const day9 = [
   {
-    name: "Simulation Tool",
-    startTime: "16:30",
-    endTime: "17:30",
+    name: "Python day-2",
+    startTime: "17:00",
+    endTime: "18:30",
     link: "http://ieeesbgvpce.org/",
     speaker: "Mr.abc",
-  },
+  }
+]
+
+const day10 = [
   {
-    name: "Python",
-    startTime: "16:30",
-    endTime: "19:30",
+    name: "Python day-3",
+    startTime: "17:00",
+    endTime: "18:30",
     link: "http://ieeesbgvpce.org/",
     speaker: "Mr.abc",
-  },
+  }
+]
+
+const day11 = [
   {
-    name: "Web Dev",
-    startTime: "18:00",
-    endTime: "19:30",
+    name: "Python day-4",
+    startTime: "17:00",
+    endTime: "18:30",
     link: "http://ieeesbgvpce.org/",
     speaker: "Mr.abc",
   }
@@ -50,24 +137,22 @@ let modalContent = (ev) => {
   myModal.setAttribute('id', 'myModel');
   let modalContent = document.createElement('div');
   modalContent.classList.add('modal-content');
-  // let spn = document.createElement('span');
-  // spn.classList.add('close');
-  // spn.setAttribute('id', 'close');
-  // spn.innerHTML = "&times;";
   let title = document.createElement('h2');
   title.innerHTML = `${ev.name}`;
   let para = document.createElement('p');
   para.innerHTML = `Time: ${ev.startTime} - ${ev.endTime}`;
   let fee = document.createElement('p');
   fee.innerHTML = "Fee : Rs.250(ieee member)\ Rs.350(non-ieee member)";
+  let speak = document.createElement('p');
+  speak.innerHTML = `<br>Speaker: ${ev.speaker}`;
   let lnk = document.createElement('a');
   lnk.innerHTML = "Register" + "<br>";
   let suggestion = document.createElement('small');
   suggestion.innerHTML = "click on outside to close modal";
   lnk.href = `${ev.link}`;
-  // modalContent.append(spn);
   modalContent.append(title);
   modalContent.append(para);
+  modalContent.append(speak);
   modalContent.append(fee);
   modalContent.append(lnk);
   modalContent.append(suggestion);
@@ -125,23 +210,55 @@ const createEventElement = (event, start, end) => {
 const switchDays = (day) => {
   switch (day) {
     case 1:
+      if(!isEmpty)
+        clearFun();
+      day1.forEach(locateCoords);
+      break;
     case 3:
+      if(!isEmpty)
+        clearFun();
+      day3.forEach(locateCoords);
+      break;
     case 5:
+      if(!isEmpty)
+        clearFun();
+      day5.forEach(locateCoords);
+      break;
     case 7:
+      if(!isEmpty)
+        clearFun();
+      day7.forEach(locateCoords);
+      break;
     case 9:
+      if(!isEmpty)
+        clearFun();
+      day9.forEach(locateCoords);
+      break;
     case 11:
+      if(!isEmpty)
+        clearFun();
+      day11.forEach(locateCoords);
+      break;
+    case 2:
+      if(!isEmpty)
+        clearFun();
+      day2.forEach(locateCoords);
+      break;
+    case 4:
       if(!isEmpty)
         clearFun();
       day4.forEach(locateCoords);
       break;
-    case 2:
-    case 4:
     case 6:
+      if(!isEmpty)
+        clearFun();
+      day6.forEach(locateCoords);
+      break;
     case 8:
     case 10:
       if(!isEmpty)
         clearFun();
-      day7.forEach(locateCoords);
+      day10.forEach(locateCoords);
       break;
     default:
       break;
